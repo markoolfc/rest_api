7e611b24ee1e235f5bf13cff6d40ff02f6330cee
markoolfc <markoolfc@gmail.com>
2020-05-15 11:28:52 +0200

    Init 3

--- rest_api.py
+++ rest_api.py
@@ -7,16 +7,19 @@ import logging
 class Api(object):
     def __init__(self, host = "http://localhost", api_key=None, api_key_prefix = 'Bearer ', ssl_ca_cert = None,log_dest = 'api.log'):
         if api_key is None:
-            self.headers = {}
+            self.headers_content = {}
         else:
-            self.headers = {'Authorization': '{0} {1}'.format(api_key_prefix,api_key)} 
+            self.headers_content = {'Content-Type':'application/json',
+               'Authorization': 'Bearer {}'.format(api_key)})
         self.host = host
         self.ssl_ca_cert = ssl_ca_cert
         logging.basicConfig(filename=log_dest,level=logging.DEBUG)
+        
     
     def vm_action(self, namespace, name, action):
         url = f"/apis/subresources.kubevirt.io/v1alpha3/namespaces/{namespace}/virtualmachines/{name}/{action}"
-        requests.put(self.host + url, headers=self.headers, verify=self.ssl_ca_cert)
+        r = requests.put(self.host + url, headers=self.headers_content, verify=self.ssl_ca_cert)
+        logging.debug(r.headers)
 
     def test(self,url):
         response = requests.get(self.host + url, headers=self.headers, verify=self.ssl_ca_cert)
